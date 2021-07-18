1fde41e4468805ed3910ef0039413a5cd80d5a1e
Yimmy Alexander Diaz <Yimy Alexander Diaz@DESKTOP-DSLPE8P>
2021-07-18 12:36:43 -0500

    Nuevos enemigos para el campo

---
+++ Ghost Adventures/Assets/Scripts/Granada.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Granada : MonoBehaviour
+{
+    public GameObject Particula;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.tag== "Player" || other.tag== "Enemigos"){
+            this.Particula.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
